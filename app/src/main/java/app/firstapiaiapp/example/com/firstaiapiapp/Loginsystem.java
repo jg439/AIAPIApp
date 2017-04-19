a3b09cdc2d011fdd1a2a53401e696715ba144c09
Julia Garcia <jg439@njit.edu>
2017-04-19 13:55:31 -0400

    adding design changes, and settings page

---
+++ app/src/main/java/app/firstapiaiapp/example/com/firstaiapiapp/Loginsystem.java
@@ -0,0 +1,14 @@
+package app.firstapiaiapp.example.com.firstaiapiapp;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class Loginsystem extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loginsystem);
+    }
+
+}
