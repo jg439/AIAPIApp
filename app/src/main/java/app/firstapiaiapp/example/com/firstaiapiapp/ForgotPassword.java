12f8901cf10cd67d54cb575b4f7ad37a05f358e4
Julia Garcia <jg439@njit.edu>
2017-05-03 14:11:46 -0400

    adding new links and new pages to the app

---
+++ app/src/main/java/app/firstapiaiapp/example/com/firstaiapiapp/ForgotPassword.java
@@ -0,0 +1,14 @@
+package app.firstapiaiapp.example.com.firstaiapiapp;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class ForgotPassword extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+    }
+
+}
--- app/src/main/java/app/firstapiaiapp/example/com/firstaiapiapp/MainActivity.java
+++ app/src/main/java/app/firstapiaiapp/example/com/firstaiapiapp/MainActivity.java
@@ -1,5 +1,7 @@
 package app.firstapiaiapp.example.com.firstaiapiapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.Manifest;
@@ -19,10 +21,15 @@ import ai.api.android.AIService;
 import ai.api.model.AIError;
 import ai.api.model.AIResponse;
 import ai.api.model.Result;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.gson.JsonElement;
 import java.util.Map;
 
-public class MainActivity extends AppCompatActivity implements AIListener{
+public class MainActivity extends AppCompatActivity implements AIListener {
 
 
     private Button listenButton;
@@ -31,26 +38,33 @@ public class MainActivity extends AppCompatActivity implements AIListener{
     private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
 
     private AIService aiService;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-             super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-             switch (requestCode){
-                       case REQUEST_RECORD_AUDIO_PERMISSION:
-                               if (grantResults.length > 0
-                                              && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                                       // permission was granted, yay!
-                                          }
-                               else {
-                                       // exit app
-                                             finish();
-                               }
-             }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQUEST_RECORD_AUDIO_PERMISSION:
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay!
+                } else {
+                    // exit app
+                    finish();
+                }
+        }
     }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
         listenButton = (Button) findViewById(R.id.listenButton);
         resultTextView = (TextView) findViewById(R.id.resultTextView);
 
@@ -65,7 +79,20 @@ public class MainActivity extends AppCompatActivity implements AIListener{
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO},
                     REQUEST_RECORD_AUDIO_PERMISSION);
         }
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
+
+    /**
+     * Called when the user clicks the Send button
+     */
+
+    public void sendMethod(View v){
+        startActivity(new Intent(MainActivity.this, Settings.class));
+    }
+
 
 
     public void listenButtonOnClick(final View view) {
@@ -85,7 +112,7 @@ public class MainActivity extends AppCompatActivity implements AIListener{
 
 
         // Show results in TextView.
-        resultTextView.setText("Question:\n" + result.getResolvedQuery() +"\n"+
+        resultTextView.setText("Question:\n" + result.getResolvedQuery() + "\n" +
                 "Response:\n" + result.getFulfillment().getSpeech());
 
 
@@ -95,16 +122,56 @@ public class MainActivity extends AppCompatActivity implements AIListener{
     public void onError(final AIError error) {
         resultTextView.setText(error.toString());
     }
-    @Override
-    public void onListeningStarted() {}
 
     @Override
-    public void onListeningCanceled() {}
+    public void onListeningStarted() {
+    }
 
     @Override
-    public void onListeningFinished() {}
+    public void onListeningCanceled() {
+    }
 
     @Override
-    public void onAudioLevel(final float level) {}
+    public void onListeningFinished() {
+    }
 
+    @Override
+    public void onAudioLevel(final float level) {
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Main Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
 }
